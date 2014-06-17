4776ed3787ddc6cf39abfc09e1a1f9c3b6db1531
Ricardo <odrakir@gmail.com>
2014-06-17 17:09:00 +0200

    Initial commit

---
+++ UIKitAnimations/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  UIKitAnimations
+//
+//  Created by Ricardo Sánchez Sotres on 17/06/14.
+//  Copyright (c) 2014 Ricardo Sanchez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)doAnima:(id)sender {
+}
+
+@end
