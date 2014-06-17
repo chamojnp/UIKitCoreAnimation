d103edfb5af0940a4003edef6f638a90b4e01ef3
Ricardo <odrakir@gmail.com>
2014-06-17 18:47:44 +0200

    transitionWithView

--- UIKitAnimations/ViewController.m
+++ UIKitAnimations/ViewController.m
@@ -7,35 +7,21 @@
 //
 
 #import "ViewController.h"
-#import "MiVista.h"
 
 @interface ViewController ()
-@property (nonatomic, strong) UIView* rojo;
-@property (nonatomic, strong) UIView* amarillo;
-@property (nonatomic, strong) UIView* azul;
+@property (weak, nonatomic) IBOutlet UIView *vista;
+@property (weak, nonatomic) IBOutlet UIView *subvista;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
 @end
 
 @implementation ViewController
 
-- (UIView *) creaCuadradoDeColor:(UIColor *) color
-{
-    UIView* cuadrado = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    cuadrado.backgroundColor = color;
-    [self.view addSubview:cuadrado];
-    return cuadrado;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-    self.rojo = [self creaCuadradoDeColor:[UIColor redColor]];
-    self.amarillo = [self creaCuadradoDeColor:[UIColor yellowColor]];
-    self.azul = [self creaCuadradoDeColor:[UIColor blueColor]];
-    
-    self.rojo.center = CGPointMake(120, 120);
-    self.amarillo.center = CGPointMake(120, 240);
-    self.azul.center = CGPointMake(120, 360);
     
 }
 
@@ -47,27 +33,13 @@
 }
 
 - (IBAction)doAnima:(id)sender {
-    [UIView animateWithDuration:1.0
-                          delay:0.0
-                        options:UIViewAnimationOptionCurveEaseIn
-                     animations:^{
-                         self.rojo.center = CGPointMake(self.view.bounds.size.width-120, self.rojo.center.y);
-                     }
-                     completion:nil];
-    [UIView animateWithDuration:1.0
-                          delay:0.0
-                        options:UIViewAnimationOptionCurveEaseOut
-                     animations:^{
-                         self.amarillo.center = CGPointMake(self.view.bounds.size.width-120, self.amarillo.center.y);
-                     }
-                     completion:nil];
-    [UIView animateWithDuration:1.0
-                          delay:0.0
-                        options:UIViewAnimationOptionCurveLinear
-                     animations:^{
-                         self.azul.center = CGPointMake(self.view.bounds.size.width-120, self.azul.center.y);
-                     }
-                     completion:nil];
+    [UIView transitionWithView:self.vista
+                      duration:1.0
+                       options:UIViewAnimationOptionTransitionFlipFromTop
+                    animations:^{
+                        [self.subvista removeFromSuperview];
+                        self.label.hidden = YES;
+                    } completion:nil];
 
 }
 
